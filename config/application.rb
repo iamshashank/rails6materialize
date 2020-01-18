a05f11b314507b6b0a49a4d4ba2e98e33342acc8
Anshul Kumar <anshul.kumar@daffodilsw.com>
2020-01-18 15:19:46 +0530

    refactor

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,7 @@ module MaterialCrud
     # the framework and any gems in your application.
   end
 end
+Raven.configure do |config|
+  config.dsn = 'https://16d8b74f619f4e58a19b57e9a7bb3c96:4fdd20c2446d4bb68f75460ee0a18a35@sentry.io/1886611'
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+end
